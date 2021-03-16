3695b63d3dba3fe562cdd55c2b4b4759f3582a4e
abdulfattah-zh <abdulfattah.zhilkibaev@gmail.com>
2021-03-16 15:28:40 +0600

    create: added reviews section and fix hotel slider

--- js/main.js
+++ js/main.js
@@ -1,20 +1,33 @@
-var swiper = new Swiper('.swiper-container', {
+var horelSlider = new Swiper('.hotel-slider', {
   // Optional parameters
   loop: true,
 
   // Navigation arrows
   navigation: {
-    nextEl: '.slider-button--next',
-    prevEl: '.slider-button--prev',
+    nextEl: '.hotel-slider__button--next',
+    prevEl: '.hotel-slider__button--prev',
   },
+  effect: "coverflow",
+});
+
+var reviewsSlider = new Swiper('.reviews-slider', {
+  // Optional parameters
+  loop: true,
+
+  // Navigation arrows
+  navigation: {
+    nextEl: 'reviews-slider__button--next',
+    prevEl: 'reviews-slider__button--prev',
+  },
+ 
 });
 
 document.addEventListener('keydown', function(event) {
   if (event.code == 'ArrowLeft' ) {
-    document.querySelector('.slider-button--prev').click(); 
+    document.querySelector('.hotel-slider__button--prev').click(); 
   }
   else if(event.code == 'ArrowRight'){
-    document.querySelector('.slider-button--next').click(); 
+    document.querySelector('.hotel-slider__button--next').click(); 
   }
 });
 
