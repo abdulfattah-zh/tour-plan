ab0d196405104385838bcc1046662a73b1975011
abdulfattah-zh <abdulfattah.zhilkibaev@gmail.com>
2021-03-27 18:37:21 +0600

    create: mobile menu

--- js/main.js
+++ js/main.js
@@ -1,48 +1,53 @@
-var horelSlider = new Swiper('.hotel-slider', {
+var horelSlider = new Swiper(".hotel-slider", {
   // Optional parameters
   loop: true,
 
   // Navigation arrows
   navigation: {
-    nextEl: '.hotel-slider__button--next',
-    prevEl: '.hotel-slider__button--prev',
+    nextEl: ".hotel-slider__button--next",
+    prevEl: ".hotel-slider__button--prev",
   },
- 
 });
 
-var reviewsSlider = new Swiper('.reviews-slider', {
+var reviewsSlider = new Swiper(".reviews-slider", {
   // Optional parameters
   loop: true,
 
   // Navigation arrows
   navigation: {
-    nextEl: 'reviews-slider__button--next',
-    prevEl: 'reviews-slider__button--prev',
+    nextEl: "reviews-slider__button--next",
+    prevEl: "reviews-slider__button--prev",
   },
- 
 });
 
-document.addEventListener('keydown', function(event) {
-  if (event.code == 'ArrowLeft' ) {
-    document.querySelector('.hotel-slider__button--prev').click(); 
-  }
-  else if(event.code == 'ArrowRight'){
-    document.querySelector('.hotel-slider__button--next').click(); 
+document.addEventListener("keydown", function (event) {
+  if (event.code == "ArrowLeft") {
+    document.querySelector(".hotel-slider__button--prev").click();
+  } else if (event.code == "ArrowRight") {
+    document.querySelector(".hotel-slider__button--next").click();
   }
 });
 
-   ymaps.ready(init);
-    function init(){
-        // Создание карты.
-        var myMap = new ymaps.Map("map", {
-            // Координаты центра карты.
-            // Порядок по умолчанию: «широта, долгота».
-            // Чтобы не определять координаты центра карты вручную,
-            // воспользуйтесь инструментом Определение координат.
-            center: [25.14122666,  55.18541349],
-          
-            // Уровень масштабирования. Допустимые значения:
-            // от 0 (весь мир) до 19.
-            zoom: 16
-        });
-    }
+ymaps.ready(init);
+function init() {
+  // Создание карты.
+  var myMap = new ymaps.Map("map", {
+    // Координаты центра карты.
+    // Порядок по умолчанию: «широта, долгота».
+    // Чтобы не определять координаты центра карты вручную,
+    // воспользуйтесь инструментом Определение координат.
+    center: [25.14122666, 55.18541349],
+
+    // Уровень масштабирования. Допустимые значения:
+    // от 0 (весь мир) до 19.
+    zoom: 16,
+  });
+}
+
+var menuButton = document.querySelector(".menu-button");
+menuButton.addEventListener("click", function () {
+  console.log("кнопка клик");
+  document
+    .querySelector(".navbar-bottom")
+    .classList.toggle("navbar-bottom--visible");
+});
